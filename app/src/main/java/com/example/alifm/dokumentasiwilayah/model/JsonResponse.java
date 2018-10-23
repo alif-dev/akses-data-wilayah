3c4f3661f48ce8098d7dd9447c7367a3d731a1e3
Alif Muqtadir <alifm.dev@gmail.com>
2018-10-23 13:05:09 +0800

    GET API DATA button removed

--- app/src/main/java/com/example/alifm/dokumentasiwilayah/model/JsonResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.alifm.dokumentasiwilayah.model;
-
-public class JsonResponse {
-    private Region[] province;
-
-    public Region[] getProvince() {
-        return province;
-    }
-}
